626f18201f0a0b990363b8235e5d4105e6d1cbed
batthatozzybit <00001093757231sp@al.educacao.sp.gov.br>
2024-11-06 08:27:15 -0300

    roblox

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Banda',
+    'Quais As Bandas Brasileiras Mais Famosas?',
+    'Sepultura, Angra, Capital Inicial, Pitty, Titans, Os Mutantes etc'
+)
+
+criaCartao(
+    'JoJo',
+    'Quais São os viloes de JoJo?',
+    'Dio, Kars, Kira, Diavolo, Pucci, Valentine, Toru'
+)
+
+criaCartao(
+    'Fnaf',
+    'Quais os nomes Das Crianças Mortas?',
+    'Gabriel, Susie, Jeremy, Fritz e Cassidy'
+)
+
+criaCartao(
+    'Alanzoka',
+    'Quantos Anos Tem O alan?',
+    '18'
+)
